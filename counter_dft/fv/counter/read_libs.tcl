a07be4d41b9c8cfda3c97f44283fb33ff24c86e0
jagadeesh-pradhani <01fe21bee114@kletech.ac.in>
2025-02-20 00:26:34 -0500

    Stop tracking fv/ folders

--- counter_dft/fv/counter/read_libs.tcl
+++
@@ -1,4 +0,0 @@
-add_search_path . /cad_area/install/GENUS211/tools.lnx86/lib/tech -library -both
-read_library -liberty -both \
-    /home/adld21ee114/DFT/DFT/counter_dft/LIB/slow.lib
-
--- mcrb/fv/mcrb/read_libs.tcl
+++
@@ -1,4 +0,0 @@
-add_search_path . /cad_area/install/GENUS211/tools.lnx86/lib/tech -library -both
-read_library -liberty -both \
-    /cad_area/install/FOUNDRY/digital/90nm/dig/lib/slow.lib
-
